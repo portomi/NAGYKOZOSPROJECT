2a6fb6dab8a6354df77af7a03c9055daf6dca304
portomi <38259146+portomi@users.noreply.github.com>
2018-07-02 00:37:48 +0200

    getting rid of redundant distance matrix

--- Eucledian_distance_of_cells .py
+++ Eucledian_distance_of_cells .py
@@ -10,42 +10,33 @@ ROISs is a dictionary containing an array containing a dictionary
 """
 import pickle
 import pandas as pd
-from scipy.spatial.distance import pdist, squareform
+from scipy.spatial.distance import pdist
 
 with open('rois.pkl', 'rb') as f:
     data = pickle.load(f)
 
-#Calculates centroid of each individual polygon
-def Centroid(poly):
-    poly = pd.DataFrame(poly)
-    return poly.mean(axis = 0).tolist()
-    
+centroids = [] # list for ROI centroids
+labels = [] # list for labels of ROIs
 
-#list for ROI centroids
-centroids = []
-#list for labels of ROIs
-labels = []
-
-#Iterate data to calculate each centroid and store it in "centroids" list
+"""Iterate data to calculate each centroid and store it in "centroids" list"""
 for i in range(len(data['ROIs']['rois'])):
     #polygons is a 1 element array so [0] after ['polygons'] should stay 0
     centroids.append(Centroid(data['ROIs']['rois'][i]['polygons'][0]))
     labels.append(str(data['ROIs']['rois'][i]['label']))
 
 
-#convert to Dataframe with ROI labels as indexes
+"""convert to Dataframe with ROI labels as indexes"""
 centroids = pd.DataFrame(centroids)
 centroids.insert(loc=0, column='label', value=labels)
 centroids.set_index('label')
 centroids.columns = ['label', 'x', 'y', 'z']
 
-#sets distance of 'z' according to the measurement plane distances
+"""sets distance of 'z' according to the measurement plane distances"""
 centroids.loc[centroids['z']==1, 'z'] = 12.5
 centroids.loc[centroids['z']==2, 'z'] = 25
 
-#create distance matrics
+"""create distance matrics"""
 distances = pdist(centroids.values[:,1:4], metric='euclidean')
-dist_matrix = squareform(distances)
-dist_matrix = pd.DataFrame(dist_matrix, index=labels, columns=labels)
-#dist_matrix.to_csv('Distance_matrix.csv', encoding='ascii')
-
+distances = pd.DataFrame(distances, index=list(itertools.combinations(labels, 2)),
+                         columns = ['distance'])
+distances.sort_values('distance')
